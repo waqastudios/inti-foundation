cec83d3b4dfaa4431bc8ae60999c95f059c38901
Stuart Starrs <stuart@waqastudios.com>
2015-12-23 16:45:21 -0500

    Fixed search button pushing out of wrapper slightly, noticable more when its in a sidebar to the left with content to the right. Smaller button looks much better, but, let's just not have overflowing on parent theme.

--- searchform.php
+++ searchform.php
@@ -10,11 +10,11 @@
 <form role="search" method="get" id="searchform" action="<?php echo home_url(); ?>">
 	<div class="row collapse">
 		<label class="screen-reader-text" for="s"><?php _e('Search for:', 'inti'); ?></label>
-		<div class="small-9 columns">
+		<div class="small-8 columns">
 			<input type="text" value="<?php get_search_query(); ?>" name="s" id="s" placeholder="<?php echo esc_attr__('Search', 'inti'); ?>" />
 		</div>
-		<div class="small-3 columns end">
-			<input class="button prefix" type="submit" id="searchsubmit" value="<?php echo esc_attr__('Search', 'inti'); ?>" />
+		<div class="small-4 columns">
+			<input class="button expanded" type="submit" id="searchsubmit" value="<?php echo esc_attr__('Search', 'inti'); ?>" />
 		</div>
 	</div>
 </form>
