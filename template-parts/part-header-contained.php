2669c58691962f518358338f5d519f1434bb7bda
Stuart Starrs <stuart@waqastudios.com>
2016-12-20 18:51:49 -0500

    Made some slight improvements to headers and navigation

--- template-parts/part-header-contained.php
+++ template-parts/part-header-contained.php
@@ -7,7 +7,13 @@
 	<div id="site-banner" class="site-banner<?php if ( !get_inti_option('show_site_banner_mobile', 'inti_customizer_options') ) echo " show-for-mlarge"; ?>" role="banner">
 		<div class="row">
 			<?php inti_hook_site_banner_site_logo_before(); ?>
-			<?php if ( get_inti_option('logo_image', 'inti_customizer_options') ) : ?>
+			<?php  
+			/**
+			* Add logo or site title to the site-banner, hidden in on smaller screens where another logo is shown on top-bar
+			*/
+			$mainbanner = get_inti_option('logo_image', 'inti_customizer_options');
+
+			if ( $mainbanner ) : ?>
 			<div class="site-logo">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 					<?php inti_do_srcset_image(get_inti_option('logo_image', 'inti_customizer_options'), esc_attr( get_bloginfo('name', 'display') . ' logo')); ?>
--- template-parts/part-header-wide.php
+++ template-parts/part-header-wide.php
@@ -8,7 +8,13 @@
 
 		<div class="column">
 			<?php inti_hook_site_banner_site_logo_before(); ?>
-			<?php if ( get_inti_option('logo_image', 'inti_customizer_options') ) : ?>
+			<?php  
+			/**
+			* Add logo or site title to the site-banner, hidden in on smaller screens where another logo is shown on top-bar
+			*/
+			$mainbanner = get_inti_option('logo_image', 'inti_customizer_options');
+
+			if ( $mainbanner ) : ?>
 			<div class="site-logo">
 				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
 					<?php inti_do_srcset_image(get_inti_option('logo_image', 'inti_customizer_options'), esc_attr( get_bloginfo('name', 'display') . ' logo')); ?>
