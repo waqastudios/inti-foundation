3decd03d651ee06cbdfaf34b1c5b5d886faa5ea3
Stuart Starrs <stuart@waqastudios.com>
2017-07-10 21:55:09 +0200

    Fixed contained footer layout

--- template-parts/part-footer-contained.php
+++ template-parts/part-footer-contained.php
@@ -1,13 +1,9 @@
 					<?php inti_hook_footer_before(); ?>
 
 					<footer id="site-footer-contained" class="site-footer">
-
-							<div class="grid-x grid-margin-x">
-								<div class="small-12 cell">
-									<?php inti_hook_footer_inside() ?>
-								</div>
-							</div>
-
+						<div class="grid-container">
+							<?php inti_hook_footer_inside() ?>
+						</div>
 					</footer>
 					
 					<?php inti_hook_footer_after(); ?>
